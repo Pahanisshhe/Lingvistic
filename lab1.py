cedb6af3a3ee682544630991620de67fc00113d7
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-09-26 12:42:58 +0300

    task2

--- lab1.py
+++ lab1.py
@@ -1,5 +1,15 @@
 from nltk import sent_tokenize
+from nltk import word_tokenize
 
-text = "Led ask possible mistress? relation elegance eat likewise debating." \
-gg = sent_tokenize(text)
-print(gg)
+text = "Led ask possible mistress relation elegance eat likewise debating." \
+       " By message or am nothing amongst chiefly address. The its enable direct" \
+       " men depend highly. Ham windows sixteen who inquiry fortune demands. " \
+       "Is be upon sang fond must shew. Really boy law county she unable her sister. " \
+       "Feet you off its like like six. Among sex are leave law built now. In built " \
+       "table in an rapid blush. Merits behind on afraid or warmly."
+sents = sent_tokenize(text)
+print(sents)
+
+sent = "Led ask possible mistress relation elegance eat likewise debating."
+words = word_tokenize(sent)
+print(words)
